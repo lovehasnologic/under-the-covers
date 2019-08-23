b898ebfb4223c7641960ed938e8603c0136714fd
Mike Gibson <mikegibson@Mike-Gibson-SPR-MacBook-Pro.local>
2019-08-23 14:17:23 -0500

    Rename artwork to billboard

--- src/components/Billboard.js
+++ src/components/Billboard.js
@@ -13,15 +13,15 @@ class Billboard extends React.Component {
 
   render() {
     return (
-      <header className="artwork">
-        <h1 className="artwork__title">
-          <span className="artwork__title--under">Under</span>
-          <span className="artwork__title--the">The</span>
+      <header className="billboard">
+        <h1 className="billboard__title">
+          <span className="billboard__title--under">Under</span>
+          <span className="billboard__title--the">The</span>
           <br />
-          <span className="artwork__title--covers">Covers</span>
+          <span className="billboard__title--covers">Covers</span>
         </h1>
-        <h2 className="artwork__description">
-          <strong className="artwork__count">{this.props.volumeCount}</strong>
+        <h2 className="billboard__description">
+          <strong className="billboard__count">{this.props.volumeCount}</strong>
           volumes of songs <u>not</u> written by the people playing them
         </h2>
       </header>
